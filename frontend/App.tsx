16a7e96c68f8b0cb0ccf652ab7e08a104ee29643
thomasmartuaps <thomasmartuaps@gmail.com>
2020-11-27 22:12:56 +0700

    added redux, creating views

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,13 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import store from './store/index';
+import { Provider } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './views/home'
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+
+    </Provider>
   );
 }
 
--- frontend/store/actions/index.ts
+++ frontend/store/actions/index.ts
@@ -1,5 +1,17 @@
 import axios from 'axios';
 
-const baseUrl = 'http://192.168.88.8:3000';
+// const baseUrl = 'http://192.168.88.8:3000';
 
 export const SET_USER = 'SET_USER';
+
+interface SetUserData {
+  email: string;
+  token: string;
+}
+
+export const setUser = (data: SetUserData) => {
+  return {
+    type: SET_USER,
+    payload: data,
+  }
+}
